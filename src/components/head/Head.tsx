f97e8676a42822145b46124fd5532f3d0ac4f400
Evgeni Nikolov <evgeni.tsn@gmail.com>
2021-05-08 15:55:06 +0300

    add: fonts

--- src/components/head/Head.tsx
+++ src/components/head/Head.tsx
@@ -12,6 +12,24 @@ const Head: React.FC<Props> = ({ title }) => {
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name='description' content={META_DESCRIPTION} />
+      <link
+        rel='preload'
+        href='/fonts/WorkSans/WorkSans-Bold.ttf'
+        as='font'
+        crossOrigin=''
+      />
+      <link
+        rel='preload'
+        href='/fonts/Poppins/Poppins-Light.ttf'
+        as='font'
+        crossOrigin=''
+      />
+      <link
+        rel='preload'
+        href='/fonts/Poppins/Poppins-Regular.ttf'
+        as='font'
+        crossOrigin=''
+      />
     </NextHead>
   );
 };
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+
+  /* Fonts */
+  @font-face {
+    font-family: "WorkSans";
+    src: url("/fonts/WorkSans/WorkSans-Bold.ttf");
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Poppins";
+    src: url("/fonts/Poppins/Poppins-Light.ttf");
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Poppins";
+    src: url("/fonts/Poppins/Poppins-Regular.ttf");
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+
+  /* Reset css */
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -20,7 +45,7 @@ export const GlobalStyles = createGlobalStyle`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Poppins', sans-serif;
     box-sizing: border-box;
   }
   /* HTML5 display-role reset for older browsers */
