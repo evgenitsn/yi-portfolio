e31c5f7733c57bc5d4747e6e2290749210ce95d4
Evgeni Nikolov <evgeni.tsn@gmail.com>
2021-05-21 20:19:46 +0300

    add: fetch home photography data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from 'next';
+import { IHomePhotographyFields } from '../../@types/generated/contentful';
+import { getAllEntriesByContentType } from '../api/service';
 import { SectionTitle } from '../components';
 import { Layout } from '../layout';
 
-const Home: React.FC = () => {
+interface Props {
+  homePhotography: IHomePhotographyFields[];
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const homePhotography = await getAllEntriesByContentType('homePhotography');
+  return {
+    props: { homePhotography },
+  };
+};
+
+const Home: React.FC<Props> = () => {
   return (
     <Layout>
       <SectionTitle>Recent work</SectionTitle>
