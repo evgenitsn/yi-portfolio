99ae17ce4ca10dc69339a0e2b6bd1157a50d74c5
Evgeni Nikolov <evgeni.tsn@gmail.com>
2021-06-21 21:30:19 +0300

    typing: fixed tests typings

--- src/test/pages/index.test.tsx
+++ src/test/pages/index.test.tsx
@@ -4,12 +4,18 @@ import Home from '../../pages/index';
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home homePhotography={[]} />, {});
+    const { asFragment } = render(
+      <Home homePhotography={[]} homeRecent={[]} />,
+      {}
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home homePhotography={[]} />, {});
+    const { getByText } = render(
+      <Home homePhotography={[]} homeRecent={[]} />,
+      {}
+    );
     window.alert = jest.fn();
     fireEvent.click(getByText('Test Button'));
     expect(window.alert).toHaveBeenCalledWith('With typescript and Jest');
